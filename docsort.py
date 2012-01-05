caa906f8073b5801c0bd2b5f8d096ac231c3e5aa
Joni Kähärä <joni.kahara@gmail.com>
2012-01-05 10:09:27 +0200

    add command line parsing

--- docsort.py
+++ docsort.py
@@ -3,17 +3,21 @@
 import os, sys, shutil, datetime
 
 class DocSort:
-    def __init__(self, source_dir, target_dir, file_age=40320):
+    def __init__(self, source_dir, target_dir, file_age):
         for f in os.listdir(source_dir):
-
-            # xxx only process files older than file_age (seconds) ago
+            
             file_name = source_dir + '/' + f
             if not os.path.isfile(file_name):
                 print file_name, 'is not a regular file'
                 continue
+
             print 'Processing', file_name
             mtime = int(os.stat(file_name).st_mtime)
             date = datetime.datetime.fromtimestamp(mtime)
+
+            if (datetime.datetime.utcnow() - date) < datetime.timedelta(0, file_age):
+                print 'Skipping', file_name, '(%s)' % (date, )
+                continue
             
             dir = target_dir + '/' + '%04d' % (date.year, )
             if not os.path.exists(dir):
@@ -29,8 +33,28 @@ class DocSort:
                 e = sys.exc_info()[1]
                 print(str(e))
 
+def usage():
+    print 'usage: %s source_dir target_dir <age_seconds>' % (sys.argv[0], )
+    sys.exit(2)
+    
 if __name__ == '__main__':
-    dir = os.getcwd()
-    print dir
-    ds = DocSort(dir, dir)
 
+    if len(sys.argv) < 3:
+        usage()
+
+    if len(sys.argv) > 3:
+        try:
+            age_seconds = int(sys.argv[3])
+        except:
+            usage()
+    else:
+        age_seconds = 2419200
+    
+    source_dir = sys.argv[1]
+    target_dir = sys.argv[2]
+    for dir in [source_dir, target_dir]:
+        if not os.path.isdir(dir) or not os.access(dir, os.R_OK):
+            usage()
+
+    ds = DocSort(source_dir, target_dir, age_seconds)
+    
